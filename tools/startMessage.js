153eb31048f8889eab2fdbbb012e1ee5eecf6d09
Jeff Pagley <jeffpagley@gmail.com>
2018-08-12 18:58:09 -0400

    Changes by Saturday Branch

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -2,5 +2,5 @@ import colors from 'colors';
 
 /* eslint-disable no-console */
 
-console.log('Starting app in dev mode...',colors.red);
-
+console.log('Starting app in dev mode...',colors.green);
+console.log('Change by the Saturday Branch in startMessage.js...',colors.red);
